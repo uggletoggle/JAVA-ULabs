a503219216563bd96944b8d44f4fbe5814ca5c79
Boya <boya_18_09@hotmail.com>
2018-08-03 20:04:52 -0300

    Metodo insert en PersonaJDBC

--- src/datos/PersonaJDBC.java
+++ src/datos/PersonaJDBC.java
@@ -11,5 +11,34 @@ public class PersonaJDBC {
     private final String SQL_DELETE = "DELETE FROM persona WHERE id_persona=?";
     private final String SQL_SELECT = "SELECT * FROM persona ORDER BY id_persona";
     
+    public int insert(String nombre, String apellido){
     
+        Connection conn = null;
+        PreparedStatement pps = null;
+        int rows = 0;
+        
+        try{
+        
+            conn = Conexion.getConnection();
+            pps = conn.prepareStatement(SQL_INSERT);
+            pps.setString(1, nombre);
+            pps.setString(2, apellido);
+            System.out.println("Ejecutando Query: " + SQL_INSERT);
+            rows = pps.executeUpdate();
+            System.out.println("Registros afectados: " + rows);
+            
+        }catch(SQLException e){
+        
+            e.printStackTrace();
+        
+        } finally{
+            
+            Conexion.close(pps);
+            Conexion.close(conn);
+                        
+        }
+        
+        return rows;
+        
+    }
 }
