becec49803d8e5a9ba0732cace45e561566baf8d
Boya <boya_18_09@hotmail.com>
2018-08-03 20:49:01 -0300

    Metodo update en PersonaJDBC

--- src/datos/PersonaJDBC.java
+++ src/datos/PersonaJDBC.java
@@ -41,4 +41,37 @@ public class PersonaJDBC {
         return rows;
         
     }
+    
+    public int update (int id_persona, String nombre, String apellido){
+    
+        Connection conn = null;
+        PreparedStatement pps = null;
+        ResultSet rs = null;
+        int rows = 0;
+        
+        try{
+        
+            conn = Conexion.getConnection();
+            pps = conn.prepareStatement(SQL_UPDATE);
+            pps.setString(1, nombre);
+            pps.setString(2, apellido);
+            pps.setInt(3, id_persona);
+            rows = pps.executeUpdate();
+            System.out.println("Registros actualizados: " + rows);
+            
+        }catch(SQLException e){
+        
+            e.printStackTrace();
+        
+        }finally{
+        
+            Conexion.close(pps);
+            Conexion.close(conn);
+            
+        }
+        
+        return rows;
+        
+    }
+    
 }
