fcc44ff012c3ce08cc963129c3c38bee4ca4920a
Boya <boya_18_09@hotmail.com>
2018-08-03 14:22:42 -0300

    Configuracion de la clase Persona y Conexion

--- src/datos/Conexion.java
+++ src/datos/Conexion.java
@@ -1,14 +1,92 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package datos;
 
-/**
- *
- * @author Carlos
- */
+import java.sql.*;
+
 public class Conexion {
+    
+    private static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static String JDBC_URL = "mysql:jdbc://localhost:3306/sga?useSSL=false";
+    private static String JDBC_USER = "root";
+    private static String JDBC_PASS = "";
+    
+    private static Driver driver = null;
+    
+    public static synchronized Connection getConnection()
+            throws SQLException{
+    
+        if (driver == null){
+        
+            try {
+            
+                Class jdbcDriverClass = Class.forName(JDBC_DRIVER);
+                driver = (Driver) jdbcDriverClass.newInstance();
+                DriverManager.registerDriver(driver);
+            
+            }catch(Exception e){
+            
+                System.out.println("Fallo en la carga del driver JDBC");
+                e.printStackTrace();
+                
+            }
+        
+        }
+        
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
+        
+    }
+    
+    public static void close(ResultSet rs){
+    
+        try{
+        
+            if (rs != null){
+            
+                rs.close();
+                
+            }
+        
+        }catch(SQLException e){
+        
+            e.printStackTrace();
+        
+        }
+    
+    }
+    
+    public static void close(PreparedStatement ps){
+    
+       try{
+       
+           if (ps != null){
+           
+               ps.close();
+               
+           }
+       
+       }catch(SQLException e){
+       
+           e.printStackTrace();
+       
+       }
+    
+    }
+    
+    public static void close(Connection c){
+    
+        try{
+        
+            if (c != null){
+            
+                c.close();
+            
+            }
+        
+        }catch(SQLException e){
+        
+            e.printStackTrace();
+        
+        }
+    
+    }
     
 }
--- src/domain/Persona.java
+++ src/domain/Persona.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domain;
 
-/**
- *
- * @author Carlos
- */
+
 public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private int id_persona;
+    
+    public int getId_persona(){
+    
+        return id_persona;
+    
+    }
+    
+    public String getNombre(){
+    
+        return nombre;
+        
+    }
+    
+    public String getApellido(){
+    
+        return apellido;
+    
+    }
+    
+    public void setId_persona(int id_persona){
+    
+        this.id_persona = id_persona;
+    
+    }
+    
+    public void setNombre(String nombre){
+    
+        this.nombre = nombre;
+    
+    }
+    
+    public void setApellido(String apellido){
+    
+        this.apellido = apellido;
+    
+    }
+    
+    @Override
+    public String toString(){
+    
+        return "{Persona: id=" + id_persona + " nombre=" + nombre + " apellido=" + apellido + '}';
+        
+    }
     
 }
