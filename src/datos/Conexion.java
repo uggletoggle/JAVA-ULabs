c9f64c464f73e8f17b47855881c52849ee4febff
Boya <boya_18_09@hotmail.com>
2018-08-05 22:08:02 -0300

    Version final corregida

--- src/datos/Conexion.java
+++ src/datos/Conexion.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Conexion {
     
     private static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private static String JDBC_URL = "mysql:jdbc://localhost:3306/sga?useSSL=false";
+    private static String JDBC_URL = "jdbc:mysql://localhost:3306/sga?useSSL=false";
     private static String JDBC_USER = "root";
     private static String JDBC_PASS = "";
     
